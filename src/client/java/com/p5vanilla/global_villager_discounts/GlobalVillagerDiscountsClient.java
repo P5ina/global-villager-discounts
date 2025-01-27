69808cfea71041cf1a931cf5bad02b5503632585
Timur Turatbekov <timatur242005@gmail.com>
2025-01-27 13:01:33 +0600

    Add client package

---
+++ src/client/java/com/p5vanilla/global_villager_discounts/GlobalVillagerDiscountsClient.java
@@ -0,0 +1,10 @@
+package com.p5vanilla.global_villager_discounts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlobalVillagerDiscountsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client entry point
+    }
+}
